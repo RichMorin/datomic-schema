04d4bf41d5544a4729d0f7e005c052b576976b43
Trevor Borg <trevor.borg@gmail.com>
2012-04-14 19:35:37 -0500

    added comment and newline support

--- parser.rb
+++ parser.rb
@@ -65,20 +65,26 @@ end
 file = File.open('schema.dtm', 'w')
 file.write('[')
 IO.foreach(ARGV[0]) do |line|
-  options = line.split
-  attr = Attribute.new(options[0..3])
-  if options.length > 4 then options[4..-1].each do |opt|
-      case opt
-      when "unique-id" then attr.unique = "identity"
-      when "unique-val" then attr.unique = "value"
-      when "index" then attr.index = "true"
-      when "fulltext" then attr.fulltext = "true"
-      when "component" then attr.isComponent = "true"
-      when "noHistory" then attr.noHistory = "true"
+  unless line.length == 1
+    if line[0] == 59
+      file.write("\n" + line)
+    else
+      options = line.split
+      attr = Attribute.new(options[0..3])
+      if options.length > 4 then options[4..-1].each do |opt|
+          case opt
+          when "unique-id" then attr.unique = "identity"
+          when "unique-val" then attr.unique = "value"
+          when "index" then attr.index = "true"
+          when "fulltext" then attr.fulltext = "true"
+          when "component" then attr.isComponent = "true"
+          when "noHistory" then attr.noHistory = "true"
+          end
+        end
       end
+      file.write(attr)
     end
   end
-  file.write(attr)
 end
 file.write(']')
 file.close
